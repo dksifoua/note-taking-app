d077ba447a7e409510b96ac1e1bc25f99e23cc71
dksifoua <dimitri.sifoua@gmail.com>
2026-03-09 08:44:55 -0400

    Add `MenuBar` component with `HomeIcon`, `TagIcon`/

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,17 +1,20 @@
 import { type JSX } from "react"
 import { Button } from "./components/Button"
-import { RefreshLeftIcon, SearchIcon } from "./components/icons"
+import { RefreshLeftIcon, SearchIcon, TagIcon } from "./components/icons"
 import { Card } from "./components/Card"
 import { InputTextGroup } from "./components/Input"
 import { Toast } from "./components/Toast"
 import { Header, HeaderControl } from "./components/Header"
+import { MenuBar } from "./components/Menu"
 
 export function App(): JSX.Element {
 
     return (
         <div className="flex flex-col gap-4 p-5">
+            <MenuBar/>
             <HeaderControl/>
             <Header/>
+            <TagIcon className="size-5 stroke-neutral-600"/>
             <SearchIcon className="size-5 fill-neutral-600"/>
             <Toast message="Note saved successfully!"/>
             <Toast message="Note archived." link="Archived Notes"/>
---
+++ apps/ui/src/components/Menu/MenuBar.tsx
@@ -0,0 +1,35 @@
+import { type JSX } from "react"
+import { ArchiveIcon, HomeIcon, SearchIcon, SettingIcon, TagIcon } from "../icons"
+
+export function MenuBar(): JSX.Element {
+
+    return (
+        <div
+            className="lg:hidden h-14 md:h-18 flex flex-row px-4 md:px-8 py-3 items-center justify-between border-t border-t-neutral-200">
+            <div className={`w-20 flex md:flex-col gap-y-1 py-1 items-center justify-center bg-blue-50 rounded-4`}>
+                <HomeIcon className={`size-6 fill-blue-500`}/>
+                <p className={`max-md:hidden text-preset-6 text-blue-500`}>Home</p>
+            </div>
+            <div className="max-md:hidden h-full border border-neutral-100"/>
+            <div className={`w-20 flex md:flex-col gap-y-1 py-1 items-center justify-center rounded-4`}>
+                <SearchIcon className={`size-6 fill-neutral-600`}/>
+                <p className={`max-md:hidden text-preset-6 text-neutral-600`}>Search</p>
+            </div>
+            <div className="max-md:hidden h-full border border-neutral-100"/>
+            <div className={`w-20 flex md:flex-col gap-y-1 py-1 items-center justify-center rounded-4`}>
+                <ArchiveIcon className={`size-6 stroke-neutral-600`}/>
+                <p className={`max-md:hidden text-preset-6 text-neutral-600`}>Archived</p>
+            </div>
+            <div className="max-md:hidden h-full border border-neutral-100"/>
+            <div className={`w-20 flex md:flex-col gap-y-1 py-1 items-center justify-center rounded-4`}>
+                <TagIcon className={`size-6 stroke-neutral-600`}/>
+                <p className={`max-md:hidden text-preset-6 text-neutral-600`}>Tags</p>
+            </div>
+            <div className="max-md:hidden h-full border border-neutral-100"/>
+            <div className={`w-20 flex md:flex-col gap-y-1 py-1 items-center justify-center rounded-4`}>
+                <SettingIcon className={`size-6 fill-neutral-600`}/>
+                <p className={`max-md:hidden text-preset-6 text-neutral-600`}>Settings</p>
+            </div>
+        </div>
+    )
+}
